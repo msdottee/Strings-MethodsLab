54d45273b8ed42b0635f797dedd9171a9aa0974c
ashley <ashleykubera@gmail.com>
2020-04-22 19:55:13 -0400

    changed class name

--- src/stringandmethodslab/StringLab.java
+++ src/stringandmethodslab/StringLab.java
@@ -17,7 +17,7 @@ public class StringLab {
 	}
 	
 	public static void letterize(String word) {
-		String newWord = Methods.capitalize(word);
+		String newWord = StringMethods.capitalize(word);
 		
 		for (int i = 0; i < newWord.length(); i++) {
 			System.out.println(newWord.charAt(i));
@@ -35,7 +35,7 @@ public class StringLab {
 		
 		System.out.println("Enter a word with random capitalization.");
 		word = scnr.next();
-		System.out.println("Abracadabra: " + Methods.capitalize(word));
+		System.out.println("Abracadabra: " + StringMethods.capitalize(word));
 		
 		System.out.println();
 		
@@ -52,7 +52,7 @@ public class StringLab {
 		a = scnr.next();
 		System.out.println("Enter another word.");
 		b = scnr.next();
-		System.out.println("Here are the words in alphabetical order: " + Methods.firstThingsFirst(a, b) + " ");
+		System.out.println("Here are the words in alphabetical order: " + StringMethods.firstThingsFirst(a, b) + " ");
 		
 		System.out.println();
 		
@@ -67,25 +67,25 @@ public class StringLab {
 		
 		System.out.println("Enter a sentence with Waldo in it.");
 		phrase = scnr.nextLine();
-		System.out.println("Waldo is at index: " + Methods.wheresWaldo(phrase));
+		System.out.println("Waldo is at index: " + StringMethods.wheresWaldo(phrase));
 		
 		System.out.println();
 		
 		System.out.println("Please give me a string (word or sentence).");
 		s = scnr.nextLine();
-		System.out.println("Your string in reverse: " + Methods.reverse(s));
+		System.out.println("Your string in reverse: " + StringMethods.reverse(s));
 		
 		System.out.println();
 		
 		System.out.println("Enter a sentence with or without the word math.");
 		phrase = scnr.nextLine();
-		System.out.println("Your sentence after method afterMath: " + Methods.afterMath(phrase));
+		System.out.println("Your sentence after method afterMath: " + StringMethods.afterMath(phrase));
 		
 		System.out.println();
 		
 		System.out.println("Enter a phrase.");
 		phrase = scnr.nextLine();
-		System.out.println("Here is your sentence in camel case: " + Methods.camelCase(phrase));
+		System.out.println("Here is your sentence in camel case: " + StringMethods.camelCase(phrase));
 		
 		System.out.println();
 		
