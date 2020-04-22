b0c6324569bf95ff3a3eb91700d1308ab929b91b
ashley <ashleykubera@gmail.com>
2020-04-22 19:00:45 -0400

    changed name of second class and organized code

---
+++ src/stringandmethodslab/Methods.java
@@ -0,0 +1,71 @@
+package stringandmethodslab;
+
+public class Methods {
+	
+	public static String capitalize(String word) {
+		String newWord;
+		
+		newWord = word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+		
+		return newWord;
+	}
+	
+	public static int wheresWaldo(String phrase) {
+		int stringIndex;
+		
+		stringIndex = phrase.indexOf("Waldo");
+		return stringIndex;
+		
+	}
+	
+	public static String firstThingsFirst(String a, String b) {
+		
+		if (a.compareToIgnoreCase(b) < 1) {
+			return a + " " + b;
+		} else if (a.compareToIgnoreCase(b) > 1) {
+			return b + " " + a;
+		} else {
+			return a + " " + b;
+		}
+	}
+	
+	public static String reverse(String s) {
+		StringBuilder s1 = new StringBuilder();
+		
+		s1.append(s);
+		
+		s1 = s1.reverse();
+		
+		return s1.toString();
+	}
+	
+	public static String afterMath(String phrase) {
+		int mathIndex;
+		String newPhrase;
+		
+		if (phrase.toLowerCase().contains("math")) {
+			mathIndex = phrase.indexOf("math");
+			newPhrase = phrase.substring(mathIndex);
+		} else {
+			newPhrase = "dud";
+		}
+		
+		return newPhrase;
+	}
+	
+	public static String camelCase(String phrase) {
+		StringBuilder result = new StringBuilder();
+		
+		String[] splitString = phrase.split(" ");
+		
+		result.append(splitString[0]);
+		
+		for(int i = 1; i < splitString.length; i++) {
+			
+			result.append(Character.toUpperCase(splitString[i].charAt(0)) + 
+					splitString[i].substring(1));
+		}
+		return result.toString();
+	}
+
+}
--- src/stringandmethodslab/StringLab.java
+++ src/stringandmethodslab/StringLab.java
@@ -3,25 +3,21 @@ package stringandmethodslab;
 import java.util.Scanner;
 
 public class StringLab {
-
-	public static void wheresWaldo(String phrase) {
-		
-	}
-	
-	public static void firstThingsFirst(String a, String b) {
-		
-	}
 	
 	public static void soLong(String a, String b) {
 		
-	}
-	
-	public static void afterMath(String phrase) {
+		if(a.length() > b.length()) {
+			System.out.println(a);
+		} else if (a.length() < b.length()) {
+			System.out.println(b);
+		} else {
+			System.out.println(a + " " + b);
+		}
 		
 	}
 	
 	public static void letterize(String word) {
-		String newWord = StringLab2.capitalize(word);
+		String newWord = Methods.capitalize(word);
 		
 		for (int i = 0; i < newWord.length(); i++) {
 			System.out.println(newWord.charAt(i));
@@ -32,26 +28,66 @@ public class StringLab {
 		Scanner scnr = new Scanner(System.in);
 		
 		String word;
-//		String phrase;
-//		String a;
-//		String b;
+		String phrase;
+		String a;
+		String b;
 		String s;
 		
-		System.out.println("Hit me with a word. Go wild with upper and lower case.");
+		System.out.println("Enter a word with random capitalization.");
 		word = scnr.next();
+		System.out.println("Abracadabra: " + Methods.capitalize(word));
 		
-		System.out.println("Abra Cadabra: " + StringLab2.capitalize(word));
+		System.out.println();
 		
-		scnr.nextLine();
-		System.out.println("Please give me a string (word or sentence).");
-		s = scnr.nextLine();
+		System.out.println("Enter a word");
+		a = scnr.next();
+		System.out.println("Enter another");
+		b = scnr.next();
+		System.out.println("The longer word is: ");
+		soLong(a, b);
 		
-		System.out.println("Your string in reverse: " + StringLab2.reverse(s));
+		System.out.println();
 		
-		System.out.println("Please enter a new word");
+		System.out.println("Enter a word");
+		a = scnr.next();
+		System.out.println("Enter another word.");
+		b = scnr.next();
+		System.out.println("Here are the words in alphabetical order: " + Methods.firstThingsFirst(a, b) + " ");
+		
+		System.out.println();
+		
+		System.out.println("Please enter a word");
 		word = scnr.next();
 		
 		letterize(word);
+		
+		System.out.println();
+		
+		scnr.nextLine();
+		
+		System.out.println("Enter a sentence with Waldo in it.");
+		phrase = scnr.nextLine();
+		System.out.println("Waldo is at index: " + Methods.wheresWaldo(phrase));
+		
+		System.out.println();
+		
+		System.out.println("Please give me a string (word or sentence).");
+		s = scnr.nextLine();
+		System.out.println("Your string in reverse: " + Methods.reverse(s));
+		
+		System.out.println();
+		
+		System.out.println("Enter a sentence with or without the word math.");
+		phrase = scnr.nextLine();
+		System.out.println("Your sentence after method afterMath: " + Methods.afterMath(phrase));
+		
+		System.out.println();
+		
+		System.out.println("Enter a phrase.");
+		phrase = scnr.nextLine();
+		System.out.println("Here is your sentence in camel case: " + Methods.camelCase(phrase));
+		
+		System.out.println("Thanks for spending some time with me! Goodbye!");
 		
 		scnr.close();
 
